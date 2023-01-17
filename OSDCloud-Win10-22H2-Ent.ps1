ce77b285524121b2a19c1120cc3ae504be0dede7
paulsym7 <paul@symonds-consulting.co.uk>
2023-01-17 10:34:24 +0000

    Create OSDCloud-Win10-22H2-Ent.ps1

---
+++ OSDCloud-Win10-22H2-Ent.ps1
@@ -0,0 +1,15 @@
+Install-Module -Name OSD -Force
+Import-Module -Name OSD -Force
+Write-Output 'Starting OSD Cloud...'
+Write-Output 'Installing Windows 10 22H2 Enterprise'
+$Params = @{
+  OSName = 'Windows 10 22H2 x64'
+  OSEdition = 'Enterprise'
+  OSLanguage = 'en-gb'
+  OSLicense = 'Retail'
+  Firmware = '$False'
+  Screenshot = '$True'
+  SkipAutoPilot = '$True'
+  ZTI = '$True'
+}
+Start-OSDCloud @params

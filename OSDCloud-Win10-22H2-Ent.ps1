2f3044ea555067d458b2c8627cb448ef04d3c551
paulsym7 <paul@symonds-consulting.co.uk>
2023-01-17 15:31:33 +0000

    Update OSDCloud-Win10-22H2-Ent.ps1

--- OSDCloud-Win10-22H2-Ent.ps1
+++ OSDCloud-Win10-22H2-Ent.ps1
@@ -3,7 +3,7 @@ Install-Module -Name OSD -Force
 Import-Module -Name OSD -Force
 Write-Output 'Starting OSD Cloud...'
 Write-Output 'Installing Windows 10 22H2 Enterprise'
-
+<#
 $Params = @{
   OSName = 'Windows 10 22H2 x64'
   OSEdition = 'Enterprise'
@@ -16,8 +16,8 @@ $Params = @{
   ZTI = $true
 }
 Start-OSDCloud @params
+#>
 
-<#
 $Params = @{
     OSVersion   = 'Windows 10'
     OSBuild     = '22H2'
@@ -27,6 +27,7 @@ $Params = @{
 $FeatureUpdate = Get-FeatureUpdate @Params
 $OSDImageFile = Find-OSDCloudFile -Name $FeatureUpdate.FileName -Path '\OSDCloud\OS\' | Sort-Object FullName | Where-Object {$_.Length -gt 3GB}
 $ImageFileItem = $OSDImageFile | Where-Object {$_.FullName -notlike "C*"} | Where-Object {$_.FullName -notlike "X*"} | Select-Object -First 1
+Write-Output "Using '$ImageFileItem' as the image file item."
 
 $Global:MyOSDCloud = @{
 	ImageFileItem = $ImageFileItem
@@ -46,4 +47,4 @@ $Global:MyOSDCloud = @{
 }
 
 Invoke-OSDCloud
-#>
+
